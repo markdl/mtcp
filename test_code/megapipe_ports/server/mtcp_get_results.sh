8ff77efb89df1071ad443e279cb5a52133341adc
Mark Lippincott <markdlippincott@gmail.com>
2017-08-16 17:21:06 -0700

    Updated to print results even if not enough lines

--- test_code/megapipe_ports/server/mtcp_get_results.sh
+++ test_code/megapipe_ports/server/mtcp_get_results.sh
@@ -3,10 +3,10 @@
 min=60
 lines=`grep ALL $1 | wc -l`
 
-if [ "$lines" -ge "$min" ]
+if [ "$lines" -lt "$min" ]
 then
-	 grep Total $1 | tail -n $min | awk '{ sum += $NF } END { print "msgs/s:", sum / NR }'
-	 grep ALL $1 | tail -n $min | awk '{ rx += $11; tx += $14 } END { print "rx:", rx / NR; print "tx:", tx / NR; print "all:", (rx+tx) / NR }'
-else
-	 printf "Not enough lines. Found: %d, minimum: %d\n" $lines $min 
+	 printf "Warning! Not enough lines. Found: %d, minimum: %d\n" $lines $min
 fi
+
+grep Total $1 | tail -n $min | awk '{ sum += $NF } END { print "msgs/s:", sum / NR }'
+grep ALL $1 | tail -n $min | awk '{ rx += $11; tx += $14 } END { print "rx:", rx / NR; print "tx:", tx / NR; print "all:", (rx+tx) / NR }'
